6deb606238cbecec6c773a15b7f79e52aff86e0d
Filipe Silva <silvam.filipe@gmail.com>
2017-07-27 00:12:43 +0000

    Added cache item implementation

--- spec/CacheItemSpec.php
+++ spec/CacheItemSpec.php
@@ -9,6 +9,7 @@
 
 namespace spec\Slick\Cache;
 
+use Psr\Cache\CacheItemInterface;
 use Slick\Cache\CacheItem;
 use PhpSpec\ObjectBehavior;
 
@@ -30,6 +31,11 @@ class CacheItemSpec extends ObjectBehavior
         $this->shouldHaveType(CacheItem::class);
     }
 
+    function its_a_cache_item()
+    {
+        $this->shouldHaveType(CacheItemInterface::class);
+    }
+
     function it_returns_the_key_of_current_cached_item()
     {
         $this->getKey()->shouldBe('key');
@@ -38,5 +44,47 @@ class CacheItemSpec extends ObjectBehavior
     function it_retrieves_the_value_of_the_item()
     {
         $this->get()->shouldBe('value');
+    }
+
+    function it_returns_null_when_item_is_a_miss()
+    {
+        $this->expiresAt(new \DateTimeImmutable('-1 day'));
+        $this->get()->shouldBeNull();
+    }
+
+    function it_confirms_cache_lookup_is_a_hit()
+    {
+        $this->isHit()->shouldBe(true);
+    }
+
+    function it_can_set_the_expiration_time_from_the_present()
+    {
+        $item = $this->expiresAfter(1);
+        $item->shouldBe($this->getWrappedObject());
+        sleep(2);
+        $item->isHit()->shouldBe(false);
+    }
+
+    function it_can_set_the_exactly_expiration_date_time()
+    {
+        $inOneMin = (new \DateTimeImmutable('now'))->add(new \DateInterval('PT1M'));
+        $this->expiresAt($inOneMin)->shouldBe($this->getWrappedObject());
+        $this->isHit()->shouldBe(true);
+    }
+
+    function it_can_set_the_cached_value()
+    {
+        $object = (object) ['foo' => 2, 'bar' => 'test'];
+        $this->set($object)->shouldBe($this->getWrappedObject());
+        $this->get()->shouldBe($object);
+    }
+
+    function its_serializable()
+    {
+        $this->shouldHaveType(\Serializable::class);
+        $deserialized = unserialize(serialize($this->getWrappedObject()));
+        $this->get()->shouldBe($deserialized->get());
+        $this->getKey()->shouldBe($deserialized->getKey());
+        $this->isHit()->shouldBe($deserialized->isHit());
     }
 }
--- src/CacheItem.php
+++ src/CacheItem.php
@@ -9,13 +9,15 @@
 
 namespace Slick\Cache;
 
+use Psr\Cache\CacheItemInterface;
+
 /**
  * CacheItem
  *
  * @package Slick\Cache
  * @author  Filipe Silva <silvam.filipe@gmail.com>
 */
-class CacheItem
+class CacheItem implements CacheItemInterface, \Serializable
 {
     /**
      * @var string
@@ -28,15 +30,24 @@ class CacheItem
     private $value;
 
     /**
+     * @var \DateTimeImmutable
+     */
+    private $expires;
+
+    /**
      * Cache Item
      *
      * @param string $key
-     * @param mixed  $value
+     * @param mixed $value
+     * @param null|int|\DateInterval|\DateTimeInterface $expires
      */
-    public function __construct(string $key, $value)
+    public function __construct(string $key, $value = null, $expires = null)
     {
         $this->key = $key;
         $this->value = $value;
+        ($expires instanceof \DateTimeInterface)
+            ? $this->expiresAt($expires)
+            : $this->expiresAfter($expires);
     }
 
     /**
@@ -64,6 +75,118 @@ class CacheItem
      */
     public function get()
     {
-        return $this->value;
+        return $this->isHit()
+            ? $this->value
+            : null;
+    }
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param int|\DateInterval|null $time
+     *   The period of time from the present after which the item MUST be considered
+     *   expired. An integer parameter is understood to be the time in seconds until
+     *   expiration.
+     *   If none or null is set, the value should be stored permanently or for as long as the
+     *   implementation allows.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAfter($time)
+    {
+        if (null === $time) {
+            $this->expires = null;
+            return $this;
+        }
+
+        $time = $time instanceof \DateInterval
+            ? $time
+            : new \DateInterval("PT{$time}S");
+
+
+        $this->expires = $this->now()->add($time);
+
+        return $this;
+    }
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param \DateTimeInterface|null $expiration
+     *   The point in time after which the item MUST be considered expired.
+     *   If null is passed explicitly, a default value MAY be used. If none is set,
+     *   the value should be stored permanently or for as long as the
+     *   implementation allows.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAt($expiration)
+    {
+        $this->expires = $expiration;
+        return $this;
+    }
+
+    /**
+     * Confirms if the cache item lookup resulted in a cache hit.
+     *
+     * @return bool
+     *   True if the request resulted in a cache hit. False otherwise.
+     */
+    public function isHit()
+    {
+        return null === $this->expires
+            ? true
+            : $this->now() <= $this->expires;
+    }
+
+    /**
+     * Get current date time
+     *
+     * @return \DateTimeImmutable
+     */
+    private function now()
+    {
+        return new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
+    }
+
+    /**
+     * Sets the value represented by this cache item.
+     *
+     * @param mixed $value
+     *   The serializable value to be stored.
+     *
+     * @return static
+     *   The invoked object.
+     */
+    public function set($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serialize()
+    {
+        return serialize([
+            'key' => $this->key,
+            'value' => $this->value,
+            'expires' => $this->expires
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function unserialize($serialized)
+    {
+        $data = unserialize($serialized);
+
+        $this->key = $data['key'];
+        $this->value = $data['value'];
+        $this->expires = $data['expires'];
     }
 }
