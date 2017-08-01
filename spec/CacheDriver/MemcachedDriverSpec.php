82e99622067c1940373ef2e1d36da5a4df9abb15
Filipe Silva <filipe.silva@sata.pt>
2017-08-01 19:08:17 +0000

    Start memcached driver

---
+++ spec/CacheDriver/MemcachedDriverSpec.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of slick/cache package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Slick\Cache\CacheDriver;
+
+use Slick\Cache\CacheDriver\MemcachedDriver;
+use PhpSpec\ObjectBehavior;
+use Slick\Cache\CacheDriverInterface;
+use Slick\Cache\CacheItem;
+
+/**
+ * MemcachedDriverSpec specs
+ *
+ * @package spec\Slick\Cache\CacheDriver
+ * @author  Filipe Silva <silvam.filipe@gmail.com>
+ */
+class MemcachedDriverSpec extends ObjectBehavior
+{
+    function let(\Memcached $memcachedServer)
+    {
+        $this->beConstructedWith($memcachedServer);
+    }
+
+    function it_is_initializable_with_a_memcached_instance()
+    {
+        $this->shouldHaveType(MemcachedDriver::class);
+    }
+
+    function its_a_cache_driver()
+    {
+        $this->shouldHaveType(CacheDriverInterface::class);
+    }
+
+    function it_set_a_value_in_the_cache(\Memcached $memcachedServer)
+    {
+        $value = 'test';
+        $this->set('test', $value)->shouldBe(true);
+        $memcachedServer->set('test', $value, null)
+            ->shouldHaveBeenCalled();
+    }
+
+    function it_set_a_value_with_expiration_date(
+        \Memcached $memcachedServer
+    )
+    {
+        $date = (new \DateTimeImmutable('now'))->add(new \DateInterval('PT80S'));
+        $this->set('test', 'test', $date);
+        $memcachedServer->set('test', 'test', 80)
+            ->shouldHaveBeenCalled();
+    }
+
+    function it_gets_a_value_from_the_cache(\Memcached $memcachedServer)
+    {
+        $memcachedServer->get('test')->shouldBeCalled()
+            ->willReturn('hello');
+        $this->get('test')->shouldBe('hello');
+    }
+
+    function it_returns_a_default_cache_item_if_not_found(\Memcached $memcachedServer)
+    {
+        $memcachedServer->get('test')->shouldBeCalled()
+            ->willReturn(false);
+        $expected = serialize(new CacheItem('test'));
+        $this->get('test')->shouldBe($expected);
+    }
+
+    function it_checks_the_existence_of_a_given_value(\Memcached $memcachedServer)
+    {
+        $memcachedServer->get('test')->willReturn('test');
+        $this->has('test')->shouldBe(true);
+    }
+}
---
+++ src/CacheDriver/MemcachedDriver.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * This file is part of slick/cache package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Slick\Cache\CacheDriver;
+
+use Slick\Cache\CacheDriverInterface;
+use Slick\Cache\CacheItem;
+use Slick\Cache\Exception\KeyNotFoundException;
+
+/**
+ * MemcachedDriver
+ *
+ * @package Slick\Cache\CacheDriver
+ * @author  Filipe Silva <silvam.filipe@gmail.com>
+*/
+class MemcachedDriver implements CacheDriverInterface
+{
+    /**
+     * @var \Memcached
+     */
+    private $server;
+
+    /**
+     * MemcachedDriver
+     *
+     * @param \Memcached $server
+     */
+    public function __construct(\Memcached $server)
+    {
+        $this->server = $server;
+    }
+
+    /**
+     * Stores a cache value
+     *
+     * @param string $key
+     * @param string $serializedValue
+     * @param \DateTimeImmutable $expires
+     *
+     * @return bool
+     */
+    public function set(string $key, string $serializedValue, \DateTimeImmutable $expires = null)
+    {
+        $this->server->set($key, $serializedValue, $this->calculateExpire($expires));
+        return true;
+    }
+
+    /**
+     * Gets cache item saved with provided key
+     *
+     * You should check value existence with CacheDriverInterface::has() before
+     * calling this method, otherwise an exception will be thrown.
+     *
+     * @param string $key
+     *
+     * @throws KeyNotFoundException
+     *      If the key was not found or previously saved.
+     *
+     * @return string a serialized version of cached item
+     */
+    public function get(string $key): string
+    {
+        $default = serialize(new CacheItem($key));
+        $value = $this->server->get($key);
+
+        return $value === false
+            ? $default
+            : $value;
+    }
+
+    /**
+     * Check if there's an item cached under provided key
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has(string $key): bool
+    {
+        return (boolean) $this->server->get($key);
+    }
+
+    /**
+     * Deletes all saved items in this cache bin
+     *
+     * @return bool
+     */
+    public function flush(): bool
+    {
+        // TODO: Implement flush() method.
+    }
+
+    /**
+     * Erases cache item stored with provided key
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function erase(string $key): bool
+    {
+        // TODO: Implement erase() method.
+    }
+
+    /**
+     * Get all stored keys
+     *
+     * @return string[]
+     */
+    public function getKeys()
+    {
+        // TODO: Implement getKeys() method.
+    }
+
+    /**
+     * Get the expiration value as defined by Memcached extension
+     *
+     * @see http://php.net/manual/en/memcached.expiration.php
+     *
+     * @param \DateTimeImmutable|null $expires
+     * @return int|null
+     */
+    private function calculateExpire(\DateTimeImmutable $expires = null)
+    {
+        if (! $expires instanceof \DateTimeImmutable) {
+            return null;
+        }
+
+        $now = new \DateTimeImmutable('now');
+        $diff = $expires->getTimestamp() - $now->getTimestamp();
+
+        return (60*60*24*30) > $diff
+            ? $diff
+            : $expires->getTimestamp();
+
+    }
+}
